c838a96ce2075597abd210af7ef12fe079b0d5a2
Bennie Copeland <bennie.r.copeland@gmail.com>
2017-06-04 21:11:30 +0200

    Project setup

---
+++ POS.Core.Tests/Program.cs
@@ -0,0 +1,30 @@
+﻿using NSpec;
+using NSpec.Domain;
+using NSpec.Domain.Formatters;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace POS.Core.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var types = Assembly.GetEntryAssembly().GetTypes();
+            var finder = new SpecFinder(types, "");
+            var tagsFilter = new Tags().Parse("");
+            var builder = new ContextBuilder(finder, tagsFilter, new DefaultConventions());
+
+            var runner = new ContextRunner(tagsFilter, new ConsoleFormatter(), false);
+            var contextCollection = builder.Contexts().Build();
+
+            var results = runner.Run(contextCollection);
+
+            if (results.Failures().Count() > 0)
+            {
+                Environment.Exit(1);
+            }
+        }
+    }
+}
